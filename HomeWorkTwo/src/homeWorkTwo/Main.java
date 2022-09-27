41438793506d20ba23ec09cd8f86c7684b6b2961
edayzn <eda.yzn5@gmail.com>
2022-09-27 23:27:16 +0300

    kodlama.io

---
+++ HomeWorkTwo/src/homeWorkTwo/Main.java
@@ -0,0 +1,15 @@
+package homeWorkTwo;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Methods methods=new Methods();
+		methods.sayýbulmaca();
+		System.out.println(methods.topla(2580, 157));
+		methods.topla(1,2,9,10);
+		String sehir= methods.sehirVer();
+		System.out.println(sehir);
+	}
+	
+
+}
---
+++ HomeWorkTwo/src/homeWorkTwo/Methods.java
@@ -0,0 +1,44 @@
+package homeWorkTwo;
+
+import java.util.Scanner;
+
+public class Methods {
+	
+	public static void sayýbulmaca() {
+		int[] sayilar = new int[] { 1, 2, 5, 7, 9, 0 };
+		System.out.print("Aranacak deðeri giriniz: ");
+		Scanner aranacakSayi = new Scanner(System.in);
+		int aranacak = aranacakSayi.nextInt();
+		boolean varMi = false;
+		for (int sayi : sayilar) {
+			if (sayi == aranacak) {
+				varMi = true;
+				break;
+			}
+		}
+		if (varMi) {
+			System.out.println("Tebrikler!");
+		} else {
+			System.out.println("Tekrar Deneyin!");
+		}
+
+	}
+
+	public static int topla(int sayi1,int sayi2) {
+		return sayi1+sayi2;
+	}
+	
+	public static int topla(int... sayilar) {
+		int topla=0;
+		for (int i : sayilar) {
+			topla+=i;	
+		}
+		System.out.println(topla);
+		return topla;
+	}
+	
+	public static String sehirVer() {
+		return "Ankara";
+	}
+
+}
